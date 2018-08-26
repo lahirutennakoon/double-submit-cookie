99ad5d6f6c5d765a44e988c1745b7d8e5a069fe9
Lahiru Tennakoon <lahiru.tennakoon@my.sliit.lk>
2018-08-26 19:18:27 +0530

    Add csrf token to cookie and dom.

--- src/main/java/com/csrf/doublesubmitcookie/controllers/DoubleSubmitCookieController.java
+++ src/main/java/com/csrf/doublesubmitcookie/controllers/DoubleSubmitCookieController.java
@@ -1,5 +1,72 @@
 package com.csrf.doublesubmitcookie.controllers;
 
+import com.csrf.doublesubmitcookie.services.DoubleSubmitCookieService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.view.RedirectView;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
+import java.util.HashMap;
+
+@RestController
 public class DoubleSubmitCookieController
 {
+    // Create an object of the service class
+    private DoubleSubmitCookieService doubleSubmitCookieService;
+
+    private HashMap<String, String> hashMap;
+
+    @Autowired
+    DoubleSubmitCookieController(DoubleSubmitCookieService doubleSubmitCookieService)
+    {
+        this.doubleSubmitCookieService = doubleSubmitCookieService;
+    }
+
+    @PostMapping("/login")
+    public RedirectView loginUser(@RequestParam String email, @RequestParam String password, HttpServletResponse response)
+    {
+        // Create cookie if login credentials are valid and return true
+        if (this.doubleSubmitCookieService.loginUser(email,password))
+        {
+            // Generate sessionId and csrf token
+            String sessionId = this.doubleSubmitCookieService.generateUniqueId();
+            String csrfToken = this.doubleSubmitCookieService.generateUniqueId();
+
+            // Save values to hashmap
+            hashMap = this.doubleSubmitCookieService.saveToHashMap(email, password, sessionId);
+
+            // Store sessionId and csrf token as cookies in the browser
+            Cookie sessionIdCookie = new Cookie("sessionId", sessionId);
+            Cookie csrfTokenCookie = new Cookie("csrfToken", csrfToken);
+
+            response.addCookie(sessionIdCookie);
+            response.addCookie(csrfTokenCookie);
+
+            // Redirect to new page
+            return new RedirectView("/form.html");
+        }
+
+        // Else redirect to same page with error message false
+        return new RedirectView("/?loginStatus=invalid");
+    }
+
+    @PostMapping("/checkCsrfTokenValidity")
+    public boolean checkCsrfTokenValidity(@RequestParam String sessionId, @RequestParam String csrfTokenInCookie,
+                                          @RequestParam String csrfTokenInMessageBody)
+    {
+        String sessionIdFromHashMap = hashMap.get("sessionId");
+
+        // Check if the session is valid
+        if(sessionId.equals(sessionIdFromHashMap))
+        {
+            // Return true if the 2 csrf tokens match
+            return csrfTokenInCookie.equals(csrfTokenInMessageBody);
+        }
+
+        // Return false if the 2 csrf tokens do not match
+        return false;
+    }
 }
--- src/main/java/com/csrf/doublesubmitcookie/services/DoubleSubmitCookieService.java
+++ src/main/java/com/csrf/doublesubmitcookie/services/DoubleSubmitCookieService.java
@@ -1,5 +1,34 @@
 package com.csrf.doublesubmitcookie.services;
 
+import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.UUID;
+
+@Service
 public class DoubleSubmitCookieService
 {
+    public boolean loginUser(String email, String password)
+    {
+        // Return true if login credentials are valid else return false
+        return (email.equals("admin@gmail.com")) && (password.equals("admin"));
+    }
+
+    // Generate a unique random value
+    public String generateUniqueId()
+    {
+        UUID uuid = UUID.randomUUID();
+        return uuid.toString();
+    }
+
+    public HashMap saveToHashMap(String email, String password, String sessionId)
+    {
+        HashMap<String, String> hashMap = new HashMap<>();
+
+        hashMap.put("email", email);
+        hashMap.put("password", password);
+        hashMap.put("sessionId", sessionId);
+
+        return hashMap;
+    }
 }
